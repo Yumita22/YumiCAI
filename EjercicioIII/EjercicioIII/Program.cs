32c564829fd59b0fcd3205db103b7ce1053758cc
Yumita22 <45908376+Yumita22@users.noreply.github.com>
2020-04-15 21:30:44 -0300

    Ejercicio3
    Ej3

---
+++ EjercicioIII/EjercicioIII/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = string.Empty;
+            Console.WriteLine("Escribi una palabra");
+            string s = Console.ReadLine();
+            int i = s.Length;
+            for (int j = i - 1; j >= 0; j--)
+            {
+                str = str + s[j];
+            }
+            if (str == s)
+            {
+                Console.WriteLine(s + " es un palindromo");
+            }
+            else
+            {
+                Console.WriteLine(s + " no es palindromo");
+            }
+            Console.Read();
+        }
+    }
+}
+    
+
