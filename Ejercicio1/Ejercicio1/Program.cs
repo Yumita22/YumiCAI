72d9b24c8c34e79b1c8cb89147113e8c05dcb8e8
Yumita22 <45908376+Yumita22@users.noreply.github.com>
2020-04-17 15:01:46 -0300

    Ej1

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        public static int i;
+
+        static void Main(string[] args)
+        {
+            int a = 114;
+            for (int i = 1; i < a; i++) 
+            { 
+                Console.Write(i);
+                {
+                    if (i % 3 == 0)
+                        Console.Write(" - Foo");
+                    if (i % 5 == 0)
+                        Console.Write(" - Bar");
+                }
+                Console.WriteLine(" ");
+                Console.Read();
+            }
+        }
+    }   
+
+}
