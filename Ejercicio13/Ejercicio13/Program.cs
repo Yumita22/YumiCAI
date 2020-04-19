35314e2ae157ed3c02becc8cd02148fd16479663
Yumita22 <45908376+Yumita22@users.noreply.github.com>
2020-04-19 03:57:16 -0300

    Ejercicio13

---
+++ Ejercicio13/Ejercicio13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio XIII Crear una aplicación que, cuando el usuario ingrese un número, 
+            //calcule el factorial del mismo y lo muestre por pantalla. 
+
+            int a; int factorial = 1;
+            Console.WriteLine("ingrese un numero");
+            a= int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= a; i++) 
+                {
+                factorial = factorial * i;
+                };
+            Console.WriteLine("El factorial de {0} es {1}", a, factorial);
+            Console.ReadKey();
+        }
+    }
+}
